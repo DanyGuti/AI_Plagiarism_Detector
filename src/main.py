de281fa62945c3681f8842faf5ae8e0307804f84
Dani Gutiérrez <a01068056@tec.mx>
2025-05-27 00:33:22 -0600

    Add data augment

--- src/main.py
+++ src/main.py
@@ -22,6 +22,9 @@ from models.cnn_model import (
 from features.ast_embedding import (
     ast_embedding
 )
+from preprocessing.data_augmentor import (
+    augment_data_directory
+)
 
 CASES: list[str] = [f"case-0{i}" for i in range(1,7)]
 
@@ -73,34 +76,41 @@ if __name__ == "__main__":
     # print("Matrix generation and dataset split completed.")
 
     # Load the dataset to train the model
-    train_base_path = os.path.join(os.getcwd(), "..", "matrix_data", "train")
-    validation_base_path = os.path.join(os.getcwd(), "..", "matrix_data", "validation")
-    test_base_path = os.path.join(os.getcwd(), "..", "matrix_data", "test")
-    # Load all cases for training
-    case_features, case_labels = load_all_cases(train_base_path)
-    print("Training labels shape:", case_labels.shape)
-    # Load all cases for validation
-    case_features_val, case_labels_val = load_all_cases(validation_base_path)
-    # Load all cases for testing
-    case_features_test, case_labels_test = load_all_cases(test_base_path)
+    # train_base_path = os.path.join(os.getcwd(), "..", "matrix_data", "train")
+    # validation_base_path = os.path.join(os.getcwd(), "..", "matrix_data", "validation")
+    # test_base_path = os.path.join(os.getcwd(), "..", "matrix_data", "test")
+    # # Load all cases for training
+    # case_features, case_labels = load_all_cases(train_base_path)
+    # print("Training labels shape:", case_labels.shape)
+    # # Load all cases for validation
+    # case_features_val, case_labels_val = load_all_cases(validation_base_path)
+    # # Load all cases for testing
+    # case_features_test, case_labels_test = load_all_cases(test_base_path)
 
-    # Extract the features
-    inputs = prepare_model_inputs(case_features, name_prefix="ast")
-    val_inputs = prepare_model_inputs(case_features_val, name_prefix="ast")
-    ast_embedding_model = ast_embedding("ast")
-    # pass the inputs to the model
-    ast_output = ast_embedding_model(inputs)
+    # # Extract the features
+    # inputs = prepare_model_inputs(case_features, name_prefix="ast")
+    # val_inputs = prepare_model_inputs(case_features_val, name_prefix="ast")
+    # ast_embedding_model = ast_embedding("ast")
+    # # pass the inputs to the model
+    # ast_output = ast_embedding_model(inputs)
 
-    model = binary_plagiarism_code_prediction(
-        ast_embedding_model,
-        case_labels,
-        input_data=inputs,
-        val_data=(val_inputs, case_labels_val)
+    # model = binary_plagiarism_code_prediction(
+    #     ast_embedding_model,
+    #     case_labels,
+    #     input_data=inputs,
+    #     val_data=(val_inputs, case_labels_val)
+    # )
+
+    # test_inputs = prepare_model_inputs(case_features_test, name_prefix="ast")
+    # results = model.evaluate(
+    #     x=test_inputs,
+    #     y=case_labels_test,
+    # )
+    # print(f"Test results: {results}")
+    
+    #############################
+    # PROCESS DATA AUGMENTATION #
+    #############################
+    augment_data_directory(
+       src_csv_path="/Users/Guty/Downloads/conplag/versions"
     )
-
-    test_inputs = prepare_model_inputs(case_features_test, name_prefix="ast")
-    results = model.evaluate(
-        x=test_inputs,
-        y=case_labels_test,
-    )
-    print(f"Test results: {results}")
